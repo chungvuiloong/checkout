1892e1e4c22153bd5628ddbd9eb3511f80ed9c89
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-02 23:31:14 +0200

    Refactored for easy add of new nav.

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import logo from '../../../public/assets/logo/Haiilo.png';
 
 const Header = () => {
+    const nav = ['supermarket']
     return (
         <section className='h-25 bg-primary py-4 flex flex-col justify-center'>
             <div className='container mx-auto px-4 flex items-center'>
                 <img src={logo} alt="Logo" className="h-12 mr-6" />
                 <nav>
-                    <span className="text-2xl text-white font-medium">Supermarket</span>
+                    {nav.map((item) => (
+                        <span
+                            key={item}
+                            className="capitalize text-2xl text-white font-medium"
+                        >
+                            {item}
+                        </span>
+                    ))}
                 </nav>
             </div>
         </section>
