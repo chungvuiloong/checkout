334cee0a64e6838d2ab13006cab5e17691cdfd63
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-04 21:31:39 +0200

    Created a function that updates value of an item in supermarket storage.

--- src/utils/helpers/cartUtils.tsx
+++ src/utils/helpers/cartUtils.tsx
@@ -1,4 +1,5 @@
 import type { CartItem } from '../types/cart';
+import type { Product } from '../types/product';
 
 export function inStorage(storage: CartItem[], itemId: number): boolean {
     return storage.some((item) => item.id === itemId);
@@ -15,4 +16,22 @@ export function updateItemQuantity(storage: CartItem[], itemId: number, change: 
                 } : item)
                 .filter((item) => item.quantity > 0)
     }
+}
+
+export function updateItemStorageQuantity(
+    storage: Product[],
+    itemId: number,
+    change: 'increase' | 'decrease',
+    quantity: number
+): number | undefined {
+    const itemIndex = storage.findIndex((item) => item.id === itemId);
+    if (itemIndex !== -1) {
+        switch (change) {
+            case 'increase':
+                return (storage[itemIndex].quantityInStock += quantity);
+            case 'decrease':
+                return (storage[itemIndex].quantityInStock -= quantity);
+        }
+    }
+    return undefined;
 }
