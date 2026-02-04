6ef342935ee18b2d491d95d4c900080b9365a791
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-04 10:00:35 +0200

    Created a product card.

---
+++ src/components/layout/Content.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import H from '../common/H';
+
+type ProductsProps = {
+    products: Product[];
+};
+
+type Product = {
+    name: string;
+    type: string;
+    price?: number;
+    imageUrl?: string;
+    offer?: {
+        weekly?: {
+            bundle?: {
+                quantity: number;
+                price: number;
+                days: number[];
+            };
+        };
+    };
+};
+
+const Content = ({products}: ProductsProps) => {
+    return (
+        <div>
+            {products.map((product, index) => (
+                <div key={index} className='flex flex-col gap-y-8 '>
+                    <div className='relative w-75'>
+                        <div className='bg-white h-80 rounded-2xl overflow-hidden'>
+                            <img src={product.imageUrl} className="w-full h-full object-cover" />
+                        </div>
+                        <button className='absolute left-1/2 -translate-x-1/2 -bottom-7 px-6 py-3 border-2 w-max border-primary rounded-full
+                        text-primary hover:text-white
+                        bg-white hover:bg-primary
+                        transition-colors group pointer'>
+                            <img src="/assets/icon/icon-add-to-cart.svg" alt="cart" className="inline-block w-5 h-5 mr-2 -mt-1 group-hover:brightness-0 group-hover:invert transition-all" />
+                            <span>Add to Cart</span>
+                        </button>
+                    </div>
+                    <div className='flex flex-col gap-y-1'>
+                        <H level={3} className='capitalize text-lg text-zinc-400'>{product.type}</H>
+                        <H level={2} key={index} className='capitalize text-2xl font-medium'>{product.name}</H>
+                        <div className='text-2xl text-primary font-medium'>€ {product.price?.toFixed(2)}</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default React.memo(Content);
