92558a913c0adf72f8a9419b440017aecd228e86
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-04 09:28:12 +0200

    Updated apple with image url.

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,12 +6,13 @@ const data = [
     name: "apple",
     type: "fruit",
     price: 0.30,
+    imageUrl: "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&w=800",
     offer: {
       weekly: {
         bundle: {
           quantity: 2,
           price: 0.45,
-          days: [0, 1, 2] 
+          days: [0, 1, 2]
         }
       }
     }
