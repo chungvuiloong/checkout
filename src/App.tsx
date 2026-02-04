6f171d3d71418c974b2ce76064aa717edc738dca
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-04 10:26:52 +0200

    Refactor App component layout to include Content and Cart components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import Cart from "./components/layout/Cart"
 import Header from "./components/layout/Header"
+import Content from "./components/layout/Content"
+import data from "./data/products"
+// import Container from "./components/layout/Container"
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
         <Header />
-
+        <main className="py-20">
+            <div className='container mx-auto px-4 grid grid-cols-12 gap-x-12'>
+                <Content products={data} />
+                <Cart />
+            </div>
+        </main>
     </div>
   )
 }
