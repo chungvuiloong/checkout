4a626334789347fa66055966767e21f6545f2b56
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-05 15:52:50 +0200

    Refactored Content.

--- src/components/layout/Content.tsx
+++ src/components/layout/Content.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import H from '../common/H';
-import type { Product } from '../../utils/types/product';
-import { useCart } from '../../context/CartContext';
-import Button from '../common/Button';
-import DiscountTag from '../common/DiscountTag';
+import ProductCard from '../common/ProductCard';
+import type { ProductsArrayProps } from '../../utils/types/product';
 import { getAllProductTypes } from '../../utils/helpers/productUtils';
 
-type ProductsProps = {
-    products: Product[];
-};
-
-const Content = ({products}: ProductsProps) => {
-    const { addItems } = useCart();
+const Content = ({products}: ProductsArrayProps) => {
     const [selectedType, setSelectedType] = React.useState<string | null>("all");
 
     return (
         <section className='sm:col-span-6 lg:col-span-8'>
             <div className='flex flex-col lg:flex-row justify-between'>
                 <H level={1} className='text-3xl font-semibold mb-8'>Products</H>
-                {/* <div>{getAllProductTypes().join(', ')}</div> */}
                 <div className='flex flex-wrap gap-2'>
                     {getAllProductTypes().map((type, index) => (
                         <span
@@ -34,24 +26,8 @@ const Content = ({products}: ProductsProps) => {
             <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-12'>
                 {products
                     .filter(product => selectedType === "all" ? true : product.type === selectedType)
-                    .map((product, index) => (
-                    <div key={index}>
-                        <div className='relative w-75'>
-                            <DiscountTag id={product.id} />
-                            <div className='bg-white h-80 rounded-2xl overflow-hidden'>
-                                <img src={product.imageUrl} className="w-full h-full object-cover" />
-                            </div>
-                            <Button variant='secondary' belowImage text="Add to Cart" onClick={() => addItems(product.id, 1)} disabled={product.quantityInStock === 0} />
-                        </div>
-                        <div className='mt-8 flex flex-col gap-y-1'>
-                            <H level={3} className='capitalize text-lg text-zinc-400'>{product.type}</H>
-                            <H level={2} key={index} className='capitalize text-2xl font-medium'>{product.name}</H>
-                            <div className='text-2xl text-primary font-medium'>€ {product.price?.toFixed(2)}</div>
-                        </div>
-                        <div>
-                            <p className='text-gray-600'>In Stock: {product.quantityInStock}</p>
-                        </div>
-                    </div>
+                    .map((product) => (
+                        <ProductCard  product={product} />
                 ))}
             </div>
         </section>
