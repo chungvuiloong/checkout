c14bb7f13842ed1adfd0ea46ab0222629321069e
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-05 11:16:17 +0200

    Refactored & Enhance Button component with inCart state.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,6 @@
 type ButtonProps = {
     text: string;
+    inCart?: boolean;
     belowImage?: boolean;
     variant?: "primary" | "secondary";
     onClick: () => void;
@@ -9,12 +10,15 @@ type ButtonProps = {
 
 const Button = ({
     text,
+    inCart = false,
     belowImage = false,
     variant = "primary",
     onClick,
     disabled = false,
     ariaLabel
 }: ButtonProps) => {
+    const buttonSize = inCart ? 'px-2 py-2' : 'px-6 py-3';
+    const baseStyle = `flex flex-row gap-2 ${buttonSize} border-2 w-max rounded-full transition-colors group focus:outline-none cursor-pointer touch-manipulation select-none`;
     const style = {
         'primary': 'border-primary text-white bg-primary hover:opacity-80 active:opacity-90',
         'secondary': 'border-primary text-primary hover:text-white bg-white hover:bg-primary active:bg-primary active:text-white',
@@ -26,24 +30,36 @@ const Button = ({
         'secondary': 'group-hover:brightness-0 group-hover:invert group-active:brightness-0 group-active:invert',
     } as const
 
+    const getIconSrc = () => {
+        if (text.toLowerCase() === 'increase') return '/assets/icon/icon-plus.svg';
+        if (text.toLowerCase() === 'decrease') return '/assets/icon/icon-minus.svg';
+        if (text.toLowerCase() === 'remove') return '/assets/icon/icon-trash.svg';
+        return '/assets/icon/icon-add-to-cart.svg';
+    }
+
     return (
         <button
-            className={`${hoverBelowImage} px-6 py-3 border-2 w-max rounded-full
-            ${style[variant]} transition-colors group
-            disabled:opacity-50 disabled:cursor-not-allowed
-            focus:outline-none cursor-pointer touch-manipulation select-none`}
+            className={`${hoverBelowImage} ${baseStyle} ${style[variant]}`}
             onClick={onClick}
             disabled={disabled}
             aria-label={ariaLabel || text}
             type="button"
         >
+            {inCart ?
+            <img
+                src={getIconSrc()}
+                alt=""
+                aria-hidden="true"
+                className={`inline-block w-3 h-3 ${logoStyle[variant]}`}
+            />  :
             <img
                 src="/assets/icon/icon-add-to-cart.svg"
                 alt=""
                 aria-hidden="true"
-                className={`inline-block w-5 h-5 mr-2 -mt-1 ${logoStyle[variant]}`}
-            />
-            <span>{text}</span>
+                className={`inline-block w-5 h-5 ${logoStyle[variant]}`}
+            />}
+            {!inCart && <span>{text}</span>}
+
         </button>
     );
 };
