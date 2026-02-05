19247e3ed2f83df4e87010c0d675b72c0a33d177
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2026-02-05 16:10:10 +0200

    Refactoed ProductCard component with in-cart functionality and update product types.

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -3,9 +3,49 @@ import { useCart } from '../../context/CartContext';
 import Button from './Button';
 import DiscountTag from './DiscountTag';
 import H from './H';
+import { getFinalProductPriceById, getProductImageById, getProductNameById } from '../../utils/helpers/productUtils';
 
-const ProductCard = ({ product }: ProductCardProps) => {
-  const { addItems } = useCart();
+const ProductCard = ({ inCart = false, product }: ProductCardProps) => {
+   const { clearItem, addItems, removeItems } = useCart();
+
+  if (inCart) {
+    return (
+        <div key={product.id} className="flex gap-4 rounded-lg">
+            <div className="w-20 h-20 rounded-md overflow-hidden shrink-0">
+                <img src={getProductImageById(product.id)} alt={getProductNameById(product.id)} className="w-full h-full object-cover" />
+            </div>
+            <div className="flex-1 flex flex-col gap-2">
+                <H level={3} className="text-lg font-medium capitalize">{getProductNameById(product.id)}</H>
+                <div className="flex justify-between items-center">
+                    <div className="flex gap-1 items-center">
+                        <Button
+                            inCart
+                            text="decrease"
+                            variant="secondary"
+                            onClick={() => removeItems(product.id, 1)}
+                        />
+                        <span className="text-primary font-bold min-w-6 text-center">{product.quantity}</span>
+                        <Button
+                            inCart
+                            text="increase"
+                            variant="secondary"
+                            onClick={() => addItems(product.id, 1)}
+                        />
+                    </div>
+                    <div className="flex gap-4 items-center">
+                        <span className="font-semibold text-primary">€{(getFinalProductPriceById(product.id, product.quantity || 0) ?? 0).toFixed(2)}</span>
+                        <Button
+                            inCart
+                            text="remove"
+                            variant="secondary"
+                            onClick={() => clearItem(product.id)}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+  }
 
   return (
     <div key={product.id}>
@@ -35,7 +75,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
           {product.name}
         </H>
         <div className="text-2xl text-primary font-medium">
-          € {product.price?.toFixed(2)}
+          € {(product.price ?? 0).toFixed(2)}
         </div>
       </div>
 
--- src/utils/types/product.ts
+++ src/utils/types/product.ts
@@ -1,4 +1,5 @@
 export interface Product {
+  quantity?: number;
   id: number;
   name: string;
   type: string;
@@ -18,6 +19,7 @@ export interface Product {
 
 export type ProductCardProps = {
     product: Product;
+    inCart?: boolean;
 };
 
 export type ProductsArrayProps = {
